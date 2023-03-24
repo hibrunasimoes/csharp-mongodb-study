37c235eb711165d8c22a2c6a8ad661931bd0003d
bruna simoes moita <bruna.simoesmoita@gmail.com>
2023-03-23 23:55:12 -0300

    studies mongodb csharp

---
+++ ApiNetMongoDB/WebApiMongoDB/Controllers/ProdutosController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiMongoDB.Models;
+using WebApiMongoDB.Services;
+
+namespace WebApiMongoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ProdutoServices _produtoServices;
+
+        public ProdutosController(ProdutoServices produtoServices)
+        {
+            _produtoServices = produtoServices;
+        }
+
+        [HttpGet]
+        public async Task<List<Produto>> GetProdutos()
+            => await _produtoServices.GetAsync();
+
+        [HttpPost]
+        public async Task<Produto> PostProduto(Produto produto)
+        {
+            await _produtoServices.CreateAsync(produto);
+
+            return produto;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(string id, Produto produto)
+        {
+            await _produtoServices.UpdateAsync(id, produto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveAsync(string id)
+        {
+            await _produtoServices.RemoveAsync(id);
+            return NoContent();
+        }
+    }
+}
